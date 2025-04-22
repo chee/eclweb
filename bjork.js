c0fdb4b0bd84fb7fcec37adcc32a4e0f25fd4d88
chee <yay@chee.party>
2025-04-23 00:19:10 +0100

    netlify throws node_modules away

--- bjork.js
+++ bjork.js
@@ -1,5 +1,5 @@
-importScripts("/node_modules/xterm-pty/workerTools.js");
-onmessage = (msg) => {
-	importScripts(location.origin + "/ecl.js");
-	emscriptenHack(new TtyClient(msg.data));
-};
+importScripts("/modules/xterm-pty/workerTools.js")
+onmessage = msg => {
+	importScripts(location.origin + "/ecl.js")
+	emscriptenHack(new TtyClient(msg.data))
+}
